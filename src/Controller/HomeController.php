2d00d406abd8310689ab7f9d400a32a1838834a8
zak39 <fotia.baptiste@hotmai.com>
2024-09-21 18:07:19 +0200

    feature(controller,twig): Get all characters
    We get all characters from swapi and print them in card form.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(HttpClientInterface $httpClient): Response
     {
+        $personnages = $httpClient->request(
+            'GET',
+            'https://swapi.dev/api/people/'
+        );
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'personnages' => $personnages->toArray()['results'],
         ]);
     }
 }
